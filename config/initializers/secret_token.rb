2c02ed3be1bc6a264005137913275f9257700742
Elliot Harik <elliot.harik@gmail.com>
2012-11-18 19:19:48 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MelissaBeasonSmith::Application.config.secret_token = 'f987662ea2fb4fdaf2a78dc4934d952ee53a1d1a64138f6e8fc1c50d53fe9ee7d52c2a7a9178224744b960f9a5e2f424f9c41e9f985aa6ecf061c1cbd5dd22c3'
