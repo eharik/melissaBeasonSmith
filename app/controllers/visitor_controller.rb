e956c15670f8f60f95773305531ccaf099febc0e
Elliot Harik <elliot.harik@gmail.com>
2012-11-18 20:15:51 -0600

    initial setup

---
+++ app/controllers/visitor_controller.rb
@@ -0,0 +1,26 @@
+class VisitorController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@page_title = "Welcome!"
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update 
+	end
+
+	def destroy
+	end
+
+end
+
