032a8a2f68e77fdcdf288451eaa76ba4da180efb
Elliot Harik <elliot.harik@gmail.com>
2012-11-24 17:34:08 -0600

    google maps

--- app/assets/javascripts/visitor.js
+++ app/assets/javascripts/visitor.js
@@ -1,22 +1,39 @@
 jQuery(function(){
 	jQuery('body').delegate("div[class='specialization']", 'click', function(){
-		var data= 'specialization=' + $(this).attr('id')
+		var spec = $(this).attr('id')
+		var data = 'specialization=' + spec
 		var link = '/specialization'
 		$.ajax({
 				data: data,		      
 				url: link
 		  }).done( function()
-				{})
+				{
+				})
 	})
 })
 jQuery(function(){
 	jQuery('body').delegate("div[class='navbar_link']", 'click', function(){
-		var data= 'link=' + $(this).attr('id')
+		var spec = $(this).attr('id')
+		var data= 'link=' + spec
 		var link = '/navbar'
 		$.ajax({
 				data: data,		      
 				url: link
 		  }).done( function()
-				{})
+				{
+					if (spec === 'contact') 
+					{
+						initialize()					
+					}				
+				})
 	})
 })
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(30.057856, -95.186241),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(
+		document.getElementById('map_container'), mapOptions)
+}
