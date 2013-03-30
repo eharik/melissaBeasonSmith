101aa55299089b399d9c0f636c5d568af2701f8c
Elliot Harik <elliot.harik@gmail.com>
2013-03-30 16:10:27 -0500

    selected tab shown with arrow markers

--- app/assets/javascripts/visitor.js
+++ app/assets/javascripts/visitor.js
@@ -1,33 +1,61 @@
 jQuery(function(){
-	jQuery('body').delegate("div[class='specialization']", 'click', function(){
-		var spec = $(this).attr('id')
-		var data = 'specialization=' + spec
-		var link = '/specialization'
-		$.ajax({
-				data: data,		      
-				url: link
-		  }).done( function()
-				{
-				})
-	})
+  jQuery('body').delegate("div[class='specialization']", 'click', function(){
+    var spec = $(this).attr('id')
+    var data = 'specialization=' + spec
+    var link = '/specialization'
+    specTag( spec )
+    $.ajax({
+      data: data,		      
+      url: link
+    }).done( function()
+    {
+    })
+  })
 })
 jQuery(function(){
-	jQuery('body').delegate("div[class='navbar_link']", 'click', function(){
-		var spec = $(this).attr('id')
-		var data= 'link=' + spec
-		var link = '/navbar'
-		$.ajax({
-				data: data,		      
-				url: link
-		  }).done( function()
-				{
-					if (spec === 'contact') 
-					{
-						initialize()					
-					}				
-				})
-	})
+  jQuery('body').delegate("div[class='navbar_link']", 'click', function(){
+    var page = $(this).attr('id')
+    var data = 'link=' + page
+    var link = '/navbar'
+    navTag( page ) 
+    $.ajax({
+      data: data,		      
+      url: link
+    }).done( function()
+    {
+      if (page === 'contact') 
+      {
+        initialize()					
+      }				
+    })
+  })
 })
+function specTag ( spec ) {
+  jQuery('.specialization').each( function(){
+    var id = $(this).attr('id')
+    if ( spec === id )
+    {
+      $(this).addClass('selected')
+    }
+    else 
+    {
+      $(this).attr('class','specialization')
+    }
+  })
+}
+function navTag ( page ) {
+  jQuery('.navbar_link').each( function(){
+    var id = $(this).attr('id')
+    if ( page === id )
+    {
+      $(this).addClass('selected')
+    }
+    else 
+    {
+      $(this).attr('class','navbar_link')
+    }
+  })
+}
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(29.9432187, -95.32868165),
